d83c13f525997f7f61b78b4a16ecb2f8fc78e6a8
hensing1 <web@jhlehmann.de>
2025-09-28 12:02:28 -0700

    fixed the photo blog landing page

--- src/scripts/select_pics.ts
+++ src/scripts/select_pics.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
-import { eq, and, or, gte, inArray, isNull } from "drizzle-orm";
+import { eq, and, like, gte, inArray } from "drizzle-orm";
 import {
   albums,
   images,
@@ -8,33 +8,50 @@ import {
   imageComments,
 } from "../../db/digikam.ts";
 
+const db = drizzle(process.env.DB_FILE_NAME!);
+
+export async function getAlbumIDs(albumSelector: string) {
+  const ids = await db
+    .select({
+      albumID: albums.id,
+    })
+    .from(albums)
+    .where(like(albums.relativePath, albumSelector));
+
+  return ids.map((id) => id.albumID);
+}
+
+function getImageComments(name: string, type: number) {
+  return db
+    .select({
+      imageID: imageComments.imageid,
+      comment: imageComments.comment,
+    })
+    .from(imageComments)
+    .where(eq(imageComments.type, type))
+    .as(name);
+}
+
+/// returns a subquery for image titles
+function getImageTitles() {
+  return getImageComments("titles", 3);
+}
+
+/// returns a subquery for image captions
+function getImageCaptions() {
+  return getImageComments("captions", 1);
+}
+
 export async function selectPics(albumIDs: number[], minRating: number) {
-  const db = drizzle(process.env.DB_FILE_NAME!);
-
-  const titles = db
-    .select({
-      imageID: imageComments.imageid,
-      title: imageComments.comment,
-    })
-    .from(imageComments)
-    .where(eq(imageComments.type, 3))
-    .as("titles");
-
-  const captions = db
-    .select({
-      imageID: imageComments.imageid,
-      caption: imageComments.comment,
-    })
-    .from(imageComments)
-    .where(eq(imageComments.type, 1))
-    .as("captions");
+  const titles = getImageTitles();
+  const captions = getImageCaptions();
 
   const pics = await db
     .select({
       name: images.name,
       path: images.name,
-      title: titles.title,
-      caption: captions.caption,
+      title: titles.comment,
+      caption: captions.comment,
     })
     .from(images)
     .leftJoin(titles, eq(images.id, titles.imageID))
