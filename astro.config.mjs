95c3ab46a20cba4e2065b89c44b957ab199e6884
hensing1 <web@jhlehmann.de>
2026-08-06 19:41:07 +0200

    weg mit die Komprimierung

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.hensing1.website',
+  compressHTML: false,
   integrations: [db()]
 });
